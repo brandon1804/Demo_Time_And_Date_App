d140413ecf71c918010126de1389babfb9f2f792
BRANDON KOH WEI SHENG <18044455@myrp.edu.sg>
2020-05-14 20:03:01 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18044455/demotimeanddateapp/MainActivity.java
@@ -0,0 +1,58 @@
+package sg.edu.rp.c346.id18044455.demotimeanddateapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
+    Button btnReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset= findViewById(R.id.buttonReset);
+
+       btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+            tvDisplay.setText("Time is "+ String.format("%d:%02d", tp.getCurrentHour(),tp.getCurrentMinute()));
+           }
+       });
+
+        btnDisplayDate.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvDisplay.setText("Date is "+ String.format("%d/%d/%d", dp.getDayOfMonth(), dp.getMonth() + 1, dp.getYear()));
+            }
+        }));
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                dp.updateDate(2020,0,1);
+            }
+        });
+
+
+
+    }//end of method
+}//end of class
